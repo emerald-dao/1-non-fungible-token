109d433c3a0c4c759e6595173a5a994beea5d943
Jacob Tucker <tsnakejake@aol.com>
2022-05-27 02:26:10 -0500

    finalize readme

--- flow/config.js
+++ flow/config.js
@@ -2,7 +2,7 @@ const fcl = require("@onflow/fcl");
 
 fcl.config({
   "app.detail.title": "1-SIMPLE-NFT", // this adds a custom name to our wallet
-  "app.detail.icon": "https://i.imgur.com/nRX6VfZ.png", // this adds a custom image to our wallet
+  "app.detail.icon": "https://i.imgur.com/ux3lYB9.png", // this adds a custom image to our wallet
   "accessNode.api": process.env.NEXT_PUBLIC_ACCESS_NODE, // this is for the local emulator
   "discovery.wallet": process.env.NEXT_PUBLIC_WALLET, // this is for the local dev wallet
   "0xDeployer": process.env.NEXT_PUBLIC_CONTRACT_ADDRESS, // this auto configures `0xDeployer` to be replaced by the address in txs and scripts
